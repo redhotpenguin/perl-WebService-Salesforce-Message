f0b78ffe75364d55c61c10f2559fae39ee4a0e49
Fred Moyer <fred@redhotpenguin.com>
2013-11-27 13:50:09 -0800

    first commit

---
+++ lib/WebService/Salesforce/Message.pm
@@ -0,0 +1,185 @@
+package WebService::Salesforce::Message;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+use Moo;
+use XML::LibXML;
+
+has 'xml' => ( is => 'ro', required => 1 );
+
+has 'dom' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        XML::LibXML->load_xml( string => $self->xml );
+    }
+);
+
+has 'notifications' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        my ($node) = $self->dom->findnodes('/soapenv:Envelope/soapenv:Body');
+        my ($notifications) = $node->getChildrenByTagName('notifications');
+        return $notifications;
+    }
+);
+
+has 'organization_id' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notifications->getChildrenByTagName('OrganizationId')->[0]
+          ->textContent;
+    }
+);
+
+has 'action_id' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notifications->getChildrenByTagName('ActionId')->[0]
+          ->textContent;
+    }
+);
+
+has 'session_id' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notifications->getChildrenByTagName('SessionId')->[0]
+          ->textContent;
+    }
+);
+
+has 'enterprise_url' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notifications->getChildrenByTagName('EnterpriseUrl')->[0]
+          ->textContent;
+    }
+);
+
+has 'partner_url' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notifications->getChildrenByTagName('PartnerUrl')->[0]
+          ->textContent;
+    }
+);
+
+has 'notification' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notifications->getChildrenByTagName('Notification')->[0];
+    }
+);
+
+has 'notification_id' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notification->getChildrenByTagName('Id')->[0]->textContent;
+    }
+);
+
+has 'sobject' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        return $self->notification->getChildrenByTagName('sObject')->[0];
+    }
+);
+
+has 'object_type' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self = shift;
+        my ( $ns, $type ) =
+          split( ':', $self->sobject->getAttribute('xsi:type') );
+        return $type;
+    }
+);
+
+has 'attrs' => (
+    is      => 'ro',
+    lazy    => 1,
+    default => sub {
+        my $self       = shift;
+        my @childnodes = $self->sobject->childNodes();
+        return [
+            map  { $_->localname }
+            grep { $_->isa('XML::LibXML::Element') } @childnodes
+        ];
+    }
+);
+
+sub get {
+    my ( $self, $attr ) = @_;
+    return $self->sobject->findnodes("./sf:$attr")->[0]->textContent;
+}
+
+1;
+
+__END__
+
+
+=head1 NAME
+
+WebService::Salesforce::Message - Perl extension for Salesforce outbound messages
+
+=head1 SYNOPSIS
+
+  use WebService::Salesforce::Message;
+
+  my $xml = read_in_salesforce_soap_message();
+  my $message = WebService::Salesforce::Message->new( xml => $xml );
+
+  my $attrs = $message->attrs; # Id, other attributes of the object in the message
+  my $object_id = $message->get('Id');
+  my $organization_id = $message->organization_id;
+
+=head1 DESCRIPTION
+
+Salesforce.com can send outbound SOAP messages on status changes. Use this
+module to parse those message and inspect the object attributes.
+
+See the source for available methods. Documentation will be added in 0.02, or 
+as patches are provided.
+
+=head1 SEE ALSO
+
+L<WWW::Salesforce>
+L<SOAP::Lite>
+
+=head1 AUTHOR
+
+Fred Moyer<lt>fred@redhotpenguin.comE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2013 by iParadigms LLC
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.14.2 or,
+at your option, any later version of Perl 5 you may have available.
+
+
+=cut
---
+++ t/WebService-Salesforce-Message.t
@@ -0,0 +1,53 @@
+#/usr/bin/perl
+
+use strict;
+use warnings;
+
+use Test::More tests => 14;
+BEGIN { use_ok('WebService::Salesforce::Message') }
+
+my $xml = <<'XML';
+<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+ <soapenv:Body>
+  <notifications xmlns="http://soap.sforce.com/2005/09/outbound">
+   <OrganizationId>00De0000000pmu1EAA</OrganizationId>
+   <ActionId>04ke0000000Cb2DAAS</ActionId>
+   <SessionId>00De0000000pmu1!AQsAQJ6TzpySaKROMNTwKzcOluY5SsZ0rFz9nhA3ktEIly2om86AQtWIbEaakQ9x1lTKNohBaUhDaz2ZD3RSUDCWAGuvJbS0</SessionId>
+   <EnterpriseUrl>https://cs15.salesforce.com/services/Soap/c/29.0/00De0000000pmu1</EnterpriseUrl>
+   <PartnerUrl>https://cs15.salesforce.com/services/Soap/u/29.0/00De0000000pmu1</PartnerUrl>
+   <Notification>
+    <Id>04le0000004lnFFAAY</Id>
+    <sObject xsi:type="sf:State_Province__c" xmlns:sf="urn:sobject.enterprise.soap.sforce.com">
+     <sf:Id>a29e0000000K4P0AAK</sf:Id>
+     <sf:Abbrev__c>AR</sf:Abbrev__c>
+     <sf:Country__c>a28e00000002hMtAAI</sf:Country__c>
+     <sf:Name>Arkansas</sf:Name>
+    </sObject>
+   </Notification>
+  </notifications>
+ </soapenv:Body>
+</soapenv:Envelope>
+XML
+
+use WebService::Salesforce::Message;
+my $msg = WebService::Salesforce::Message->new( xml => $xml );
+
+isa_ok( $msg, 'WebService::Salesforce::Message' );
+cmp_ok( $msg->organization_id, 'eq', '00De0000000pmu1EAA' );
+cmp_ok( $msg->action_id,       'eq', '04ke0000000Cb2DAAS' );
+cmp_ok( $msg->session_id, 'eq',
+'00De0000000pmu1!AQsAQJ6TzpySaKROMNTwKzcOluY5SsZ0rFz9nhA3ktEIly2om86AQtWIbEaakQ9x1lTKNohBaUhDaz2ZD3RSUDCWAGuvJbS0'
+);
+cmp_ok( $msg->enterprise_url, 'eq',
+    'https://cs15.salesforce.com/services/Soap/c/29.0/00De0000000pmu1' );
+cmp_ok( $msg->partner_url, 'eq',
+    'https://cs15.salesforce.com/services/Soap/u/29.0/00De0000000pmu1' );
+cmp_ok( $msg->notification_id, 'eq', '04le0000004lnFFAAY' );
+cmp_ok( $msg->object_type,     'eq', 'State_Province__c' );
+is_deeply( $msg->attrs, [qw( Id Abbrev__c Country__c Name )] );
+cmp_ok( $msg->get('Id'),         'eq', 'a29e0000000K4P0AAK' );
+cmp_ok( $msg->get('Abbrev__c'),  'eq', 'AR' );
+cmp_ok( $msg->get('Country__c'), 'eq', 'a28e00000002hMtAAI' );
+cmp_ok( $msg->get('Name'),       'eq', 'Arkansas' );
+
